cc11c13c940a39a25328be16e1bf8e19c446ab6a
ChongjinChua <chua1@purdue.edu>
2016-09-04 15:31:59 -0400

    step 1 working

--- step1code/src/Micro.java
+++ step1code/src/Micro.java
@@ -3,22 +3,21 @@ import org.antlr.v4.runtime.*;
 public class Micro{
     public static void main(String[] args) throws Exception{
 	try{
-	    //get our lexer
-	    //lexer is of type 'TokenSource'
+	    //get our lexer(type: TokenSource)
+	    //alternative: MicroLexer lexer = new MicroLexer(new ANTLRInputStream(/* a string */);
 	    MicroLexer lexer = new MicroLexer(new ANTLRFileStream(args[0]));
-	    //alternative:
-	    //MicroLexer lexer = new MicroLexer(new ANTLRInputStream(/* a string */);
 
 	    //get a list of matched tokens
 	    CommonTokenStream tokens = new CommonTokenStream(lexer);
 
+	    //get Vocabulary using matched tokens, which is used to determine symbolic token type
+	    Vocabulary vocabulary = lexer.getVocabulary(); 
+
 	    for (Token token = lexer.nextToken(); token.getType() != Token.EOF; token = lexer.nextToken()){
-		switch(token.getType()){
-		case 3: System.out.println("token type: IDEntifier"); break;
-		}
-		if(token.getType() != 7 && token.getType() != 9){
-		    System.out.println("Value: " + token.getText());
-		}
+		//getSymbolicName method returns symbolic token type. Input to the method is integer token type
+		System.out.println("Token Type: " + vocabulary.getSymbolicName(token.getType()));
+		//print literal value
+		System.out.println("Value: " + token.getText());		
 	    }
 
 	} catch (RuntimeException e){
