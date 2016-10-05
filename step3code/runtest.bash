fee2410b19b4aec7c056ec4347d85b55b2cee168
Shengli Sui <ssui@purdue.edu>
2016-10-05 19:07:58 -0400

    Added diff command

--- step3code/runtest.bash
+++ step3code/runtest.bash
@@ -4,12 +4,14 @@ j=(1 5 6 7 8 9 11 13 14 16 18 19 20 21)
 
 while [ $i -le 13 ]
 do
-    echo "******Input${j[$i]}*******  " 
-    java -cp lib/antlr.jar:classes/ Micro testcases/input/test${j[$i]}.micro
+   # echo "******Input${j[$i]}*******  " 
+    java -cp lib/antlr.jar:classes/ Micro testcases/input/test${j[$i]}.micro > out${j[$i]}.out
     echo ""
-    echo "******Expected Output${j[$i]} is******* " 
-    cat testcases/output/test${j[$i]}.out
-    echo ""
+   # echo "******Expected Output${j[$i]} is******* " 
+   # cat testcases/output/test${j[$i]}.out
+   # echo ""
+    echo "****diff result${j[$i]},empty means good******"
+    diff out${j[$i]}.out testcases/output/test${j[$i]}.out
     ((i++))
 done
 
